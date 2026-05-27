85f03e40f8920a958c10b076d7ada1c4fcb7f1be
matthuang0130 <matthuang0130@gmail.com>
2026-05-27 20:21:59 +0800

    Update generate.py

--- script/generate.py
+++ script/generate.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import os
 
+# 自動讀取 data 資料夾內的所有 Excel
+data_folder = 'data'
+output_folder = 'dist'
+if not os.path.exists(output_folder): os.makedirs(output_folder)
+
+# 之後我們會在這裡寫入處理邏輯，把 Excel 轉換為 HTML
+print("自動轉換程序啟動中...")
